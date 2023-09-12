8ec0a45fdf272f2845fa3726014174ed5f4f912b
Lydia Dunning <lydia.dunning@gmail.com>
2023-09-12 16:11:05 -0700

    5.6, 5.7 complete

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const hideWhenExpanded = { display: expanded ? 'none' : '' }
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setExpanded(!expanded)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+console.log(blog)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleVisibility} style={hideWhenExpanded}>view</button>
+      <section style={showWhenExpanded}>
+        {blog.url}
+        <br/>
+        likes: {blog.likes} <button>like</button>
+        <br/>
+        {blog.user.username}
+        <br/>
+        <button onClick={toggleVisibility}>hide</button>
+      </section>
+    </div>  
+  )
+}
+
 
 export default Blog
